7e87b4cbaf4ee56782c320cea056a7dc3d22d22f
Khaledtf Labeb <khaledtf19@gmail.com>
2023-10-24 16:33:16 +0300

    fix ci

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -4,7 +4,7 @@ type ResponseData = {
   message: string;
 };
 
-export async function GET(req:Request) {
+export async function GET() {
   console.log("hello")
   await new Promise((r) => setTimeout(r, 1000));
   return NextResponse.json({ message: "Hello..." });
