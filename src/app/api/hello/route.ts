720d96a39e37ea161c44860947f427ed36bc9c9f
Khaledtf Labeb <khaledtf19@gmail.com>
2023-10-24 16:43:42 +0300

    fix ci

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,9 +1,5 @@
 import { NextResponse } from "next/server";
 
-type ResponseData = {
-  message: string;
-};
-
 export async function GET() {
   console.log("hello")
   await new Promise((r) => setTimeout(r, 1000));
