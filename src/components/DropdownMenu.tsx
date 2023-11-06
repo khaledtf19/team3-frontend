a746e70ae69a4c36fcd4b6e6c3202adffd0693e9
unknown <hamamhussein10@gmail.com>
2023-11-06 15:11:11 +0300

    Dropdown menu v1.0

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -42,19 +42,19 @@ export const DropdownMenu = ({
   return (
     <div className="group/select-box relative z-30">
       <div
-        className={`flex w-72 cursor-pointer items-center justify-between border border-[#E9EAF0] p-2 text-gray-100 transition hover:border-[#FFDDD1] ${cn(
+        className={`flex w-40 max-w-full cursor-pointer items-center justify-between border border-[#E9EAF0] p-2 text-xs text-gray-100 transition hover:border-[#FFDDD1] md:w-72 ${cn(
           box,
         )}`}
       >
-        <p className="text-gray-500">{cateVal}</p>
+        <p className="text-gray-500 md:text-sm">{cateVal}</p>
         <FaChevronRight
-          className={`cursor-pointer text-xs text-gray-900 transition  group-hover/select-box:rotate-90 ${cn(
+          className={`cursor-pointer text-[8px] text-gray-900 transition group-hover/select-box:rotate-90  md:text-xs ${cn(
             fontawesomeIcon,
           )}`}
         />
       </div>
       <ul
-        className={`duration-400 border-border-[#E9EAF0] relative hidden w-72 cursor-pointer border shadow-sm group-hover/select-box:block ${cn(
+        className={`duration-400 border-border-[#E9EAF0] relative hidden w-40 max-w-full cursor-pointer border shadow-sm group-hover/select-box:block md:w-72 ${cn(
           list,
         )}`}
       >
@@ -67,13 +67,13 @@ export const DropdownMenu = ({
             >
               {key}
               {typeof value === "object" ? (
-                <FaChevronRight className="float-right mt-1.5 text-end text-xs text-gray-900" />
+                <FaChevronRight className="float-right mt-1.5 text-end text-[8px] text-gray-900" />
               ) : (
                 ""
               )}
               {typeof value === "object" ? (
                 <ul
-                  className={`absolute -right-[295px] hidden w-72 p-1 text-gray-700 shadow group-hover/item:block ${cn(
+                  className={`absolute -right-[108px] hidden w-24 max-w-full p-1 text-xs  text-gray-700 shadow group-hover/item:block md:-right-[295px] md:w-72 ${cn(
                     subList,
                   )}`}
                 >
@@ -81,7 +81,7 @@ export const DropdownMenu = ({
                     return (
                       <li
                         key={index}
-                        className="p-1 transition hover:text-[#FF6636]"
+                        className="p-1 text-[12px] transition hover:text-[#FF6636] md:text-sm"
                       >
                         {subCat}
                       </li>
